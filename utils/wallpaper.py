c745b70d15e50cb1ffedaa85af6f4f330825ffe0
nanashi <ny1113548@gmail.com>
2023-12-03 21:45:19 +0530

    added html

--- utils/wallpaper.py
+++ utils/wallpaper.py
@@ -1,18 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 import random
-import  re
+import re
+
 class Wallpaper:
     def __init__(self):
-        self.session = requests.Session()
         self.wallpapers = []
 
     def wallpaper_urls(self):
         page_number = 1
 
         while True:
-            response = self.session.get(f"https://www.peakpx.com/en/search?q=anime&page={page_number}")
-            soup = BeautifulSoup(response.content, 'html.parser')
+            try:
+                with open(f"templates/file{page_number}.html", 'r', encoding='utf-8') as html_file:
+                    response = html_file.read()
+                    print(response)
+            except FileNotFoundError:
+                break
+
+            soup = BeautifulSoup(response, 'html.parser')
 
             for image in soup.find_all('img'):
                 srcset = image.get('data-srcset')
@@ -36,7 +42,15 @@ class Wallpaper:
         for link in wallpapers:
             matches = re.findall(pattern, str(link))
             if matches:
-                links.append(matches)
-        ran_first = random.randint(0,len(links))
-        return links[ran_first][0]
+                links.extend(matches)
 
+        if links:
+            ran_first = random.randint(0, len(links) - 1)
+            return links[ran_first]
+        else:
+            return None
+
+# Example usage:
+wallpaper_obj = Wallpaper()
+selected_link = wallpaper_obj.extract_links()
+print(selected_link)
